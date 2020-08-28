1d338ed8c5a5179548ec9fa7a1ea20854473e009
MeiSaM25970 <meisam25970@gmail.com>
2020-08-28 20:28:55 +0430

    push

--- src/component/ads/ads.js
+++ src/component/ads/ads.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { API_SERVER_ADDRESS } from "../../env";
 class Ad extends Component {
-  state = { closeStyle: {} };
+  state = { closeStyle: { display: "block" } };
   render() {
     return (
-      <div style={this.state.closeStyle}>
+      <div className="ad-holder" style={this.state.closeStyle}>
         <span
           onClick={() => this.setState({ closeStyle: { display: "none" } })}
         >
           x
         </span>
-        <a href={this.props.link} target="_blank">
+        <a
+          style={{ display: "block", width: "100%" }}
+          href={this.props.link}
+          target="_blank"
+        >
           <img
-            width="100%"
+            style={{ display: "block", width: "100%" }}
             height="100%"
             src={API_SERVER_ADDRESS + this.props.imageAddress}
           />
